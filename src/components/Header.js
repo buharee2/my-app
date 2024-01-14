3971d2e3582e253e0ce61ce4e6c03deb7f75e161
Buhari Aminu Adam <buhareeameenu2@gmail.com>
2024-01-14 11:55:39 +0100

    Add semantic tags as components and started working on nav bar. Also add OG tags in index.html file.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from '../images/Logo.svg'
+function Header() {
+    return (
+        <>
+            <img src={logo} alt="Little lemon logo" />
+        </>
+    )
+}
+
+export default Header;
