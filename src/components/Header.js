88ce391f08f51402147514437e8d6061e05ad65c
Buhari Aminu <buhareeameenu2@gmail.com>
2024-02-04 12:17:15 +0100

    Finished with the navigation bar for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import logo from '../images/logo.svg';
-import humburgerMenu from '../images/humburgerMenu.svg';
-import cartIcon from '../images/cartIcon.svg';
 import Nav from './Nav';
 
 function Header() {
     return (
         <div id='header'>
-            <div id="headerIcons">
-                <img id='bar' src={humburgerMenu} alt='Nav Bar' />
-                <img id='logo' src={logo} alt='Logo' />
-                <img id='cart' src={cartIcon} alt='Cart' />
-            </div>
             <Nav/>
         </div>
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,45 @@
+import logo from '../images/logo.svg';
+import humburgerMenu from '../images/humburgerMenu.svg';
+import cartIcon from '../images/cartIcon.svg';
+import React, { useEffect, useState, useRef } from 'react';
+
 function Nav() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navRef = useRef(null);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+    useEffect(() => {
+       const handleResize = () => {
+        if (window.innerWidth > 768) {
+            setMenuOpen(false);
+        }
+       };
+
+       const handleClickOutside = (event) => {
+        if (navRef.current && !navRef.current.contains(event.target) && window.innerWidth <= 768) {
+            setMenuOpen(false);
+        }
+       }
+       
+       window.addEventListener('resize', handleResize);
+       document.addEventListener('click', handleClickOutside);
+
+       return () => {
+        window.removeEventListener('resize', handleResize);
+        document.removeEventListener('click', handleClickOutside);
+       };
+    }, [menuOpen]);
+
     return (
         <>
-            <nav>
-                <ul>
-                    <li><a href='#home'>Home</a></li>
+            <nav ref={navRef} className={`navbar ${menuOpen ? "Open" : ""}`}>
+                    <img className='nav-icons menu' onClick={toggleMenu} src={humburgerMenu} alt='Menu' />
+                    <a className='logo' href='/'><img  src={logo} alt='Logo' /></a>
+                    <img className='nav-icons cart' src={cartIcon} alt='Cart' />
+                <ul  className={`nav-links ${menuOpen ? "visible" : ""}`}>
+                    <li><a href='/'>Home</a></li>
                     <li><a href='#About'>About</a></li>
                     <li><a href='#Menu'>Menu</a></li>
                     <li><a href='#Reservation'>Reservation</a></li>
