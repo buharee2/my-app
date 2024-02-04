31f436b1b0e120293b23afc1806589fe70d1ce78
Buhari Aminu <buhareeameenu2@gmail.com>
2024-02-04 22:46:04 +0100

    made some changes to the nav and started working on the banner

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <React.Fragment className="App" >
+      <Nav />
       <Header/>
       <Main/>
       <Footer/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Nav from './Nav';
+import { Link } from 'react-router-dom';
+import bannerImg from '../images/bannerImg.jpg';
 
 function Header() {
     return (
-        <div id='header'>
-            <Nav/>
-        </div>
+       <header className='header'>
+        <section>
+            <div className='banner'>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family own Mediterrenan Reataurant, focused on traditional recipes served with a modern twist.</p>
+                <Link to="/booking"><button aria-label='on-Click'>Reserve Table</button></Link>
+            </div>
+            <div className='banner-img'>
+                <img src={bannerImg} alt='Banner' />
+            </div>
+        </section>
+       </header>
     )
 }
 
